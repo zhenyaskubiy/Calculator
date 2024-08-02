fce1db18307be292bb3ab208cb59ff503f0e8818
Yevgniya Skubiy <yevgeniyaskubiy@gmail.com>
2024-08-03 00:37:06 +0300

    calculator

---
+++ calc.js
@@ -0,0 +1,89 @@
+let a = ''; //first number
+let b = ''; //second number
+let sign = ''; //знак операції
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'x', '/'];
+
+//екран
+const out = document.querySelector('.calc-screen p');
+function clearAll() {
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector('.AC').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+    // натиснута не кнопка
+    if(!event.target.classList.contains('btn')) return;
+    // натиснута  кнопка clearAll AC
+    if(event.target.classList.contains('AC')) return;
+
+    out.textContent = '';
+    // отримання натиснутої кнопи
+    const key = event.target.textContent;
+
+    // якщо натиснута 0-9 або ,
+    if(digit.includes(key)) {
+        if(b ==='' && sign ===''){
+        a+= key;
+        
+        out.textContent = a;
+        }
+        else if(a!=='' && b!=='' && finish){
+            b = key;
+            finish = false;
+            out.textContent = b;
+        }
+        else{
+            b+= key;
+            out.textContent = b;
+        }
+        console.log(a, b, sign);
+        return;
+    }
+
+    // якщо натиснутий + - х / ,
+    if(action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.log(a, b, sign);
+        return;
+    }
+
+    //натиснута дорівнює
+    if(key === '='){
+        if(b ==='') b = a;
+        switch(sign){
+            case '+':
+                a = (+a) + (+b);
+                break;
+            case '-':
+                a = (+a) - (+b);
+                break;
+            case 'x':
+                a = (+a) * (+b);
+                break;
+            case '/':
+                if (b === '0'){
+                    out.textContent = 'Помилка';
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return;
+                }
+         
+
+            a = a / b;
+            break;
+        }
+        finish = true;
+        out.textContent = a;
+        console.log(a, b, sign);
+    }
+}
